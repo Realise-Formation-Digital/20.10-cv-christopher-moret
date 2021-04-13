e828056c89511fb102e8c708b78682fe8a89d3e9
Your <christophermoret@gmail.com>
2021-04-13 15:25:27 +0200

    Clean

--- forms/contact.php
+++ forms/contact.php
@@ -1,21 +1,8 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
-
-  // Replace contact@example.com with your real receiving email address
+ 
   $receiving_email_address = 'christophermoret@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
+  $contact = "christophermoret@gmail.com";
   $contact->ajax = true;
   
   $contact->to = $receiving_email_address;
